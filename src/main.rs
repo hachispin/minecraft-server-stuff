f039e5eeaa5e681eda1149921c02662e33a69bf9
hachispin <ganalonrainier255@gmail.com>
2026-09-12 18:17:37 +0100

    make controller struct

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,118 @@
 
 use std::net::Ipv4Addr;
 
-use anyhow::{Result, anyhow, bail};
+use anyhow::{Result, bail};
 use google_cloud_compute_v1::{client::Instances, model::instance::Status};
-use google_cloud_secretmanager_v1::{client::SecretManagerService, model::SecretPayload};
+use google_cloud_secretmanager_v1::client::SecretManagerService;
 use poise::{Framework, FrameworkOptions, serenity_prelude::*};
-
-type Context<'a> = poise::Context<'a, (), anyhow::Error>;
 
 const PROJECT: &str = "project-c863d0a5-25e6-435f-8b4";
 const NAME: &str = "minecraft-server";
 const ZONE: &str = "europe-west1-c";
 const DESEC_UPDATE: &str = "https://update.dedyn.io/";
 const DESEC_DNS: &str = "https://hachispin.dedyn.io/";
+
+struct Controller {
+    instances_client: Instances,
+    secret_manager: SecretManagerService,
+}
+
+impl Controller {
+    async fn new() -> Result<Self> {
+        Ok(Self {
+            instances_client: Instances::builder().build().await?,
+            secret_manager: SecretManagerService::builder().build().await?,
+        })
+    }
+
+    /// Returns the specified secret's payload.
+    ///
+    /// This also stringifies the payload, because raw bytes are rarely useful.
+    ///
+    /// # Errors
+    ///
+    /// - If no payload is found for `secret_id`
+    /// - Other typical stuff
+    async fn get_secret(&self, secret_id: &SecretId) -> Result<String> {
+        let secret_id = secret_id.as_str();
+
+        let response = self
+            .secret_manager
+            .access_secret_version()
+            .set_name(format!(
+                "projects/{PROJECT}/secrets/{secret_id}/versions/latest"
+            ))
+            .send()
+            .await?;
+
+        let Some(payload) = response.payload else {
+            bail!("No payload found for {secret_id}");
+        };
+
+        Ok(String::from_utf8(payload.data.to_vec())?)
+    }
+
+    /// Gets the status for the Minecraft VM.
+    async fn get_status(&self) -> Result<Option<Status>> {
+        let response = self
+            .instances_client
+            .get()
+            .set_project(PROJECT)
+            .set_zone(ZONE)
+            .set_instance(NAME)
+            .send()
+            .await?;
+
+        Ok(response.status)
+    }
+
+    /// Gets the current external IPv4 address of the Minecraft VM.
+    ///
+    /// Returns the first if there are multiple. There shouldn't be multiple though.
+    async fn get_ipv4(&self) -> Result<Option<Ipv4Addr>> {
+        let response = self
+            .instances_client
+            .get()
+            .set_project(PROJECT)
+            .set_zone(ZONE)
+            .set_instance(NAME)
+            .send()
+            .await?;
+
+        let ip_string = response
+            .network_interfaces
+            .iter()
+            .flat_map(|ni| &ni.access_configs)
+            .find_map(|cfg| cfg.nat_ip.as_ref());
+
+        Ok(ip_string.and_then(|s| s.parse::<Ipv4Addr>().ok()))
+    }
+
+    /// Modifies the deSEC DNS to point to the current external IPv4 address of the Minecraft VM.
+    ///
+    /// Should be run every time the Minecraft VM starts.
+    async fn set_dns_ipv4(&self, ipv4: Ipv4Addr) -> Result<()> {
+        let token = self.get_secret(&SecretId::DeSec).await?;
+        let client = reqwest::Client::new();
+
+        let response = client
+            .get(DESEC_UPDATE)
+            .query(&[("hostname", DESEC_DNS), ("myipv4", &ipv4.to_string())])
+            .header("Authorization", format!("Token {token}"))
+            .send()
+            .await?;
+
+        response.error_for_status()?;
+
+        Ok(())
+    }
+}
+
+struct PoiseState {
+    controller: Controller,
+}
+
+type Context<'a> = poise::Context<'a, PoiseState, anyhow::Error>;
 
 /// Available secrets.
 enum SecretId {
@@ -34,96 +134,12 @@ impl SecretId {
     }
 }
 
-/// Returns the specified secret's payload.
-///
-/// This also stringifies the payload, because raw bytes are rarely useful.
-///
-/// # Errors
-///
-/// - If no payload is found for `secret_id`
-/// - Other typical stuff
-async fn get_secret(secret_id: &SecretId) -> Result<String> {
-    let secret_id = secret_id.as_str();
-    let service = SecretManagerService::builder().build().await?;
-
-    let response = service
-        .access_secret_version()
-        .set_name(format!(
-            "projects/{PROJECT}/secrets/{secret_id}/versions/latest"
-        ))
-        .send()
-        .await?;
-
-    let Some(payload) = response.payload else {
-        bail!("No payload found for {secret_id}");
-    };
-
-    Ok(String::from_utf8(payload.data.to_vec())?)
-}
-
-/// Gets the status for the Minecraft VM.
-async fn get_status() -> Result<Option<Status>> {
-    let client = Instances::builder().build().await?;
-
-    let response = client
-        .get()
-        .set_project(PROJECT)
-        .set_zone(ZONE)
-        .set_instance(NAME)
-        .send()
-        .await?;
-
-    Ok(response.status)
-}
-
-/// Gets the current external IPv4 address of the Minecraft VM.
-///
-/// Returns the first if there are multiple. There shouldn't be multiple though.
-async fn get_ipv4() -> Result<Option<Ipv4Addr>> {
-    let client = Instances::builder().build().await?;
-
-    let response = client
-        .get()
-        .set_project(PROJECT)
-        .set_zone(ZONE)
-        .set_instance(NAME)
-        .send()
-        .await?;
-
-    let ip_string = response
-        .network_interfaces
-        .iter()
-        .flat_map(|ni| &ni.access_configs)
-        .find_map(|cfg| cfg.nat_ip.as_ref());
-
-    Ok(ip_string.and_then(|s| s.parse::<Ipv4Addr>().ok()))
-}
-
-/// Modifies the deSEC DNS to point to the current external IPv4 address of the Minecraft VM.
-///
-/// Should be run every time the Minecraft VM starts.
-async fn set_dns_ipv4(ipv4: Ipv4Addr) -> Result<()> {
-    let token = get_secret(&SecretId::DeSec).await?;
-    let client = reqwest::Client::new();
-
-    let response = client
-        .get(DESEC_UPDATE)
-        .query(&[("hostname", DESEC_DNS), ("myipv4", &ipv4.to_string())])
-        .header("Authorization", format!("Token {token}"))
-        .send()
-        .await?;
-
-    response.error_for_status()?;
-
-    Ok(())
-}
-
 /// Wrapper around [`get_status`] for Discord.
 #[poise::command(slash_command)]
 async fn status(ctx: Context<'_>) -> Result<()> {
     use Status::*;
 
-    let Some(status) = get_status().await? else {
+    let Some(status) = ctx.data().controller.get_status().await? else {
         ctx.say("No status returned… :(").await?;
         return Ok(());
     };
@@ -159,7 +175,8 @@ async fn status(ctx: Context<'_>) -> Result<()> {
 async fn main() -> Result<()> {
     println!("Hello, world!");
 
-    let token = get_secret(&SecretId::Discord).await?;
+    let controller = Controller::new().await?;
+    let token = controller.get_secret(&SecretId::Discord).await?;
     let intents = GatewayIntents::non_privileged();
 
     let framework_options = FrameworkOptions {
@@ -171,10 +188,11 @@ async fn main() -> Result<()> {
         .options(framework_options)
         .setup(
             // called when discord connects. registering guild-specific may be faster?
-            |ctx, _ready, framework| {
+            move |ctx, _ready, framework| {
                 Box::pin(async move {
+                    let controller = controller;
                     poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                    Ok(())
+                    Ok(PoiseState { controller })
                 })
             },
         )
